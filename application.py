8cb0eceebfe08443e15eaf352d005bdd664e704d
Jakob Ullherr <ullherrja56179@th-nuernberg.de>
2020-04-19 14:47:18 +0200

    Add: Styles, Check if user already exists

--- application.py
+++ application.py
@@ -25,32 +25,35 @@ def index():
 def register():
     return render_template("register.html")
 
+@app.route("/register_suc", methods=["POST"])
+def success():
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if(db.execute("SELECT * FROM users WHERE (username=:username)", {"username":username}).rowcount == 0):
+        db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
+                {"username": username, "password":password})
+        db.commit()
+    else:
+        return render_template("login.html", message = "Username already exists")
+
+    return render_template("success.html", message = "SUCCESS, You are now able to log in with your provided Data")
+
 #if you choose Login you end up here
 @app.route("/login", methods=["POST"])
 def login():
     return render_template("login.html")
 
-@app.route("/success",methods=["POST"])
+@app.route("/login_suc",methods=["POST"])
 def login_success():
     username = request.form.get("username")
     password = request.form.get("password")
 
     if (db.execute("SELECT * FROM users WHERE (username=:username) AND (password=:password)", {"username":username, "password":password}).rowcount == 0):
-        return render_template("error.html", message="User not found")
+        return render_template("register.html", message="User doesn't seem to exist!")
     else:
-        return render_template("notes.html")
+        return render_template("notes.html", user=username)
 
-
-@app.route("/success", methods=["POST"])
-def success():
-    username = request.form.get("username")
-    password = request.form.get("password")
-
-    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)",
-            {"username": username, "password":password})
-    db.commit()
-
-    return render_template("success.html", message = "SUCCESS, You are registered")
 
 @app.route("/notes", methods=["POST"])
 def notes():
